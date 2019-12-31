0fa3b903d529386b94aa21cfcdd58f33606a4f5a
Sina Nik <isinanik@gmail.com>
2019-12-31 17:30:54 +0330

    change let variables to const

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.querySelector(".add-todo-form").addEventListener(
 
 // Get Task
 const getTask = () => {
-  let task = {
+  const task = {
     name: document.getElementById("task-name").value,
     inMonth: document.getElementById("todo-in-month").value,
     inDay: document.getElementById("min-in-day").value
@@ -18,8 +18,8 @@ const getTask = () => {
 };
 
 const printTask = task => {
-  let todoContainer = document.querySelector(".todo-list");
-  let todoKhod = document.createElement("li");
+  const todoContainer = document.querySelector(".todo-list");
+  const todoKhod = document.createElement("li");
   todoKhod.classList.add("todo-list__items");
   todoKhod.textContent = task.name;
   todoContainer.appendChild(todoKhod);
@@ -29,26 +29,26 @@ const printTask = task => {
 
 // Show add-todo Panel
 const showPanel = () => {
-  let addTodoBTN = document.getElementById("addTodoBTN");
+  const addTodoBTN = document.getElementById("addTodoBTN");
   addTodoBTN.classList.toggle("rotate-15");
 
-  let panel = document.getElementById("panel");
+  const panel = document.getElementById("panel");
   panel.classList.toggle("display");
 };
 
 const closePanel = () => {
   panel.classList.toggle("display");
 
-  let addTodoBTN = document.getElementById("addTodoBTN");
+  const addTodoBTN = document.getElementById("addTodoBTN");
   addTodoBTN.classList.toggle("rotate-15");
 };
 
 // Task Done [Change Color]
 
 const taskDone = () => {
-  let todoItem = document.querySelector(".todo-list__items");
+  const todoItem = document.querySelector(".todo-list__items");
   todoItem.classList.toggle("task-done");
 };
 
-// let todoLi = document.querySelector(".todo-list__items");
+// const todoLi = document.querySelector(".todo-list__items");
 // todoLi.setAttribute("onClick", "taskDone()");
